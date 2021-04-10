fafb6748b3a2fd400e99697c658949f072f8a69b
vkortikere <69329819+vkortikere@users.noreply.github.com>
2021-04-10 13:51:14 -0400

    Update main.js

--- handGesture3/main.js
+++ handGesture3/main.js
@@ -19,7 +19,7 @@ function take_snapshot()
 
 console.log('ml5 version:', ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/vuWnkZvpU/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/eBIZKIGMh/model.json',modelLoaded);
 
 function modelLoaded() {
   console.log('Model Loaded!');
